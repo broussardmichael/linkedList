1c8f0a3762de4dc8338c64ba6fe59dacffd4233b
michaelbroussard <michael_broussard@msn.com>
2020-08-15 15:32:55 -0500

    attempted to switch to arrow functions, but I didn't understand what they do with 'this'.

--- linkedList.js
+++ linkedList.js
@@ -1,7 +1,8 @@
-module.exports = (function (){
+module.exports = (function () {
     let linkedListModule = {};
+
     class Node {
-        constructor(data, nextNode = null, prevNode = null){
+        constructor(data, nextNode = null, prevNode = null) {
             this.data = data;
             this.nextNode = nextNode;
             this.prevNode = prevNode;
@@ -18,42 +19,43 @@ module.exports = (function (){
         }
     }
 
-    function validateNodeArgumentForOperations (node) {
-        if(node == null)
+    function validateNodeArgumentForOperations(node) {
+        if (node == null)
             throw "A node is required to perform operations."
     }
 
-    function validateListArgumentForOperations (list) {
-        if(list.head == null)
+    function validateListArgumentForOperations(list) {
+        if (list.head == null)
             throw "List hasn't been constructed. Build a list before work can be done."
     }
 
-    function deleteNode (node){
+    function deleteNode(node) {
         let list = this;
         validateNodeArgumentForOperations(node);
         validateListArgumentForOperations(list);
 
-        if(!list.isTheNodeInTheListByData(node))
+        if (!list.isTheNodeInTheListByData(node))
             throw "Unable to delete node, the node was not found in the list by its data.";
 
-        if(list.getNodeCountFromList() === 1) {
+        if (list.getNodeCountFromList() === 1) {
             list.head = null;
         } else {
             let nextNode = node.nextNode, prevNode = node.prevNode;
-            if(node.prevNode == null){
+            if (node.prevNode == null) {
                 nextNode.prevNode = null;
                 list.head = nextNode;
             } else {
                 prevNode = node.prevNode;
                 prevNode.nextNode = nextNode;
-                if(nextNode !== null)
+                if (nextNode !== null)
                     nextNode.prevNode = prevNode;
             }
         }
+        node = null;
         delete node;
     }
 
-    function isTheNodeInTheListByData (node){
+    function isTheNodeInTheListByData(node) {
         let list = this, found = false;
         let head = list.head;
 
@@ -63,47 +65,47 @@ module.exports = (function (){
         return checkForNodeInList(node, head);
     }
 
-    function checkForNodeInList (nodeToFind, currentNode) {
-        if(currentNode.data === nodeToFind.data)
+    function checkForNodeInList(nodeToFind, currentNode) {
+        if (currentNode.data === nodeToFind.data)
             return true;
 
-        if(currentNode.nextNode != null)
+        if (currentNode.nextNode != null)
             return checkForNodeInList(nodeToFind, currentNode.nextNode);
         else
             return false;
     }
 
-    function getTailNode () {
+    function getTailNode() {
         let list = this;
         validateListArgumentForOperations(list);
 
         let node = list.head;
-        while(node.nextNode != null) {
+        while (node.nextNode != null) {
             node = node.nextNode;
         }
         return node;
     }
 
-    function getNodeCountFromList () {
+    function getNodeCountFromList() {
         let list = this;
         validateListArgumentForOperations(list);
 
         let node = list.head;
         let count = 1;
-        while(node.nextNode != null) {
+        while (node.nextNode != null) {
             node = node.nextNode;
             count++;
         }
         return count;
     }
 
-    linkedListModule.createLinkedListFromDataArray = function(dataArray){
-        if(!Array.isArray(dataArray) || (Array.isArray(dataArray) && dataArray.length === 0))
+    linkedListModule.createLinkedListFromDataArray = (dataArray) => {
+        if (!Array.isArray(dataArray) || (Array.isArray(dataArray) && dataArray.length === 0))
             throw("A array of numbers is required.");
 
         let list = {};
-        for(let i = 0; i < dataArray.length; i++){
-            if(i === 0) {
+        for (let i = 0; i < dataArray.length; i++) {
+            if (i === 0) {
                 list = new LinkedList(new Node(dataArray[i]));
             } else {
                 let node = new Node(dataArray[i]);
